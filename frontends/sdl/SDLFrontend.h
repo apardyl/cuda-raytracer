493176639e352ed3f7bc5b1a31ca57a5af028f79
Mateusz Maćkowski <m4tx@m4tx.pl>
2018-01-02 13:56:48 +0100

    Tiny SDLFrontend.h cleanup

--- frontends/sdl/SDLFrontend.h
+++ frontends/sdl/SDLFrontend.h
@@ -2,26 +2,24 @@
 #define RAY_TRACER_SDLFRONTEND_H
 
 #include <SDL.h>
-#include <string>
 
 class SDLFrontend {
 private:
-    SDL_Window *window = nullptr;
-    SDL_Surface *screenSurface = nullptr;
-
     const int SCREEN_WIDTH = 800;
     const int SCREEN_HEIGHT = 600;
     const int FPS_CAP = 30;
     const int SCREEN_TICKS_PER_FRAME = 1000 / FPS_CAP;
+
+    SDL_Window *window = nullptr;
+    SDL_Surface *screenSurface = nullptr;
+
+    void render();
 public:
     SDLFrontend();
 
     ~SDLFrontend();
 
     void run();
-
-    void render();
 };
-
 
 #endif //RAY_TRACER_SDLFRONTEND_H
