0ff7255870307798bb9179695fc1b1e67927031b
Adam Pardyl <adam@pardyl.com>
2018-01-06 22:31:39 +0100

    Implement OBJ file scene loader

--- CUDA-RayTracer/scene/SceneLoader.cpp
+++ CUDA-RayTracer/scene/SceneLoader.cpp
@@ -1,5 +1,12 @@
 ﻿#include "SceneLoader.h"
+#include <boost/algorithm/string/predicate.hpp>
+#include "scene_loaders/ObjLoader.h"
+#include "scene_loaders/UnknownFormatException.h"
 
 Scene SceneLoader::loadFromFile(const std::string &fileName) {
-    return {0, 0};
+    if (boost::ends_with(fileName, ".obj")) {
+        return ObjLoader().load(fileName);
+    }
+
+    throw UnknowFormatException("Unknown file format: " + fileName);
 }
---
+++ CUDA-RayTracer/scene/scene_loaders/ObjLoader.h
@@ -0,0 +1,12 @@
+﻿#ifndef RAY_TRACER_OBJLOADER_H
+#define RAY_TRACER_OBJLOADER_H
+
+#include "scene/SceneLoader.h"
+#include "MtlParser.h"
+
+class ObjLoader : public SceneLoader {
+public:
+    Scene load(const std::string &filename) override;
+};
+
+#endif // RAY_TRACER_OBJLOADER_H
