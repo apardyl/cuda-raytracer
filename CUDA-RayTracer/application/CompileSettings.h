619a20c6fc5e0ec032e78c3ba88e3202f2bd383e
Adam Pardyl <adam@pardyl.com>
2018-01-10 22:01:13 +0100

    Fix CUDA and GTK detection if has_include not supported

--- CUDA-RayTracer/application/CompileSettings.h
+++ CUDA-RayTracer/application/CompileSettings.h
@@ -1,6 +1,8 @@
 #ifndef RAY_TRACER_COMPILESETTINGS_H
 #define RAY_TRACER_COMPILESETTINGS_H
 
+#if defined(__has_include)
+
 #if __has_include(<gtkmm.h>)
 #define GTK_ENABLED true
 #else
@@ -13,4 +15,13 @@
 #define CUDA_ENABLED false
 #endif
 
+#define CUDA_ENABLED true
+
+#else
+
+// Set manualy if __has_include is not supported
+#define GTK_ENABLED false
+#define CUDA_ENABLED true
+
+#endif
 #endif //RAY_TRACER_COMPILESETTINGS_H
