edffa10c69cc135cfcfa57cedbfc788a47734280
Adam Pardyl <adam@pardyl.com>
2018-01-24 00:17:02 +0100

    Enable OpenMP

--- CUDA-RayTracer/backends/ray_tracing/RayTracingOpenMP.cpp
+++ CUDA-RayTracer/backends/ray_tracing/RayTracingOpenMP.cpp
@@ -21,6 +21,7 @@ Image RayTracingOpenMP::render() {
     Resolution resolution = Resolution(width, height);
     Camera camera(Point(0, 0, -1), Point(0, math::pi<float>(), 0),
                   math::pi<float>() / 2, resolution, 1);
+#pragma omp parallel for
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
             Vector vector = camera.getPrimaryVector(x, y);
@@ -30,6 +31,7 @@ Image RayTracingOpenMP::render() {
     }
     // return Image
     data = new Color[width * height];
+#pragma omp parallel for
     for (int y = 0; y < resolution.height; ++y) {
         for (int x = 0; x < resolution.width; ++x) {
             data[width * y + x] = camera.getPixelColor(x, y);
